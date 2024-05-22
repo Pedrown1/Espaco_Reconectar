7906bf515a8442d7018aaff40d91597472cbc52c
Pedro Henrique Silva Carvalho <pedro.carvalho@doalti.corp>
2024-05-22 13:32:24 -0300

    Removendo Comentarios da Ligação com o Banco de Dados

--- Dany/config.php
+++ Dany/config.php
@@ -6,10 +6,4 @@
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-    // if($conexao->connect_errno){
-    //   echo "Erro";
-    // } else {
-    //   echo "Conexão efetuada com sucesso!";
-      
-    // }
 ?>
--- Natalia/config.php
+++ Natalia/config.php
@@ -5,11 +5,4 @@
     $dbName = 'reconectarbd';
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-    /*if($conexao->connect_errno){
-      echo "Erro";
-    } else {
-      echo "Conexão efetuada com sucesso!";
-      
-    }*/
 ?>
