79ac6906a4ddce911ec73d35446ab355c97cd05b
Pedro Henrique Silva Carvalho <pedro.carvalho@doalti.corp>
2024-06-22 21:56:01 -0300

    AutoComplete OFF

--- Natalia/AgendamentoNatalia.php
+++ Natalia/AgendamentoNatalia.php
@@ -83,7 +83,7 @@ $sql_retroativo = "UPDATE agendamentos_natalia
     <main>
     <p style="text-align: center; font-size: 18px; padding-bottom: 15px;">Natalia Agendamentos:</p>
     <div class="box-serach">
-      <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
+      <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar" autocomplete="off">
       <button onclick="searchData()" class="btn btn-search">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
